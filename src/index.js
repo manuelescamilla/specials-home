4e07cf3e01df67cb81a3837a4c8ece4899949f4d
Manuel Escamilla <manu@Manuels-Mac-mini.local>
2017-08-02 14:37:03 -0400

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
+import Headroom from 'headroom.js';
 import '../styles/global.css';
 import './hero.js';
+
+
+const myElement = document.querySelector(".header");
+// construct an instance of Headroom, passing the element
+// const headroom  = new Headroom(myElement);
+// // initialise
+// headroom.init();
+
+
+const headroom = new Headroom(document.querySelector(".header"), {
+        tolerance: 5,
+        offset : 205,
+        classes: {
+          initial: "animated",
+          pinned: "slideDown",
+          unpinned: "slideUp"
+        }
+    });
+    headroom.init();
